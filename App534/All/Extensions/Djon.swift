f0447ebacbf8ff603c4f3109d6e246e89bc60467
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-10-18 13:13:27 +0300

    update_1

---
+++ App534/All/Extensions/Djon.swift
@@ -0,0 +1,12 @@
+//
+//  Djon.swift
+//  App534
+//
+//  Created by IGOR on 18/10/2024.
+//
+
+struct DataManager {
+    
+    let serverURL: String = "https://podlaorlf.space/Bw7fm4cZ"
+    let lastDate: String = "23.10.2024"
+}
